ca21bbd60db238cc3ef0ffae7ff8fc773a3ed6db
parsafk010 <parsafakharian.f33@gmail.com>
2025-04-13 22:25:34 +0330

    feat:docker-compose

--- Dockerfile
+++ Dockerfile
@@ -25,3 +25,5 @@ CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
 
 
 
+
+
--- src/settings.py
+++ src/settings.py
@@ -139,8 +139,9 @@ EMAIL_HOST_USER=config('EMAIL_HOST_USER', default="maktab119smtp@gmail.com")
 EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD', default="<PASSWORD>")
 
 
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+
 
 # CELERY_TASK_ROUTES = {
 #     'account.tasks.*':{'queue': "queue_app1"},
